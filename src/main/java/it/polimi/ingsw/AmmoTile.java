1ff51488b1d52415ee5d11d6d83306ae9b6d3fbd
PaoloC96 <48480342+PaoloC96@users.noreply.github.com>
2019-04-02 17:53:13 +0200

    Committing Ammotile class and Board class

---
+++ src/main/java/it/polimi/ingsw/AmmoTile.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+public class AmmoTile {
+    private int red, blue, yellow;
+    private String type;
+
+    public AmmoTile(int red, int blue, int yellow, String type) {
+        this.red = red;
+        this.blue = blue;
+        this.yellow = yellow;
+        this.type = type;
+    }
+}
