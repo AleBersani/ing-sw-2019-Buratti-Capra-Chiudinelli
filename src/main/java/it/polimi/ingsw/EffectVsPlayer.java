bc1dc8adb677dc119b4b58c3389ec1867fce957f
PaoloC96 <48480342+PaoloC96@users.noreply.github.com>
2019-04-02 18:50:52 +0200

    Modified EffectVsPlayer and Weapon

--- src/main/java/it/polimi/ingsw/EffectVsPlayer.java
+++ src/main/java/it/polimi/ingsw/EffectVsPlayer.java
@@ -14,13 +14,6 @@ public class EffectVsPlayer extends Effect {
 
     private int damage,mark;
     private boolean addToList,removeFromList;
-
-    public EffectVsCharacter(int costBlue, int costRed, int costYellow, String name, ArrayList<Constraint> constraints, ArrayList<Boolean> constraintPositivity, int damage, int mark) {
-        super(costBlue, costRed, costYellow, name, constraints, constraintPositivity);
-        this.damage = damage;
-        this.mark = mark;
-    }
-
     public void apply(Player target){
 
     }
