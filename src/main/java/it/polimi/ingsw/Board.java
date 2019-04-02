ea924a1276139de61584661e60a6aba8c30b533c
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-04-02 18:14:41 +0200

    Fixed errors in Board

--- src/main/java/it/polimi/ingsw/Board.java
+++ src/main/java/it/polimi/ingsw/Board.java
@@ -3,10 +3,10 @@ package it.polimi.ingsw;
 import java.util.ArrayList;
 
 public class Board {
-    private ArrayList<Room> rooms= new ArrayList<>();
-    private ArrayList<AmmoTile> ammoList= new ArrayList<>();
-    private ArrayList<PowerUp> powerUpList= new ArrayList<>();
-    private ArrayList<Weapon> weaponsList= new ArrayList<>();
+    private ArrayList<Room> rooms= new ArrayList<Room>();
+    private ArrayList<AmmoTile> ammoList= new ArrayList<AmmoTile>();
+    private ArrayList<PowerUp> powerUpList= new ArrayList<PowerUp>();
+    private ArrayList<Weapon> weaponsList= new ArrayList<Weapon>();
 
     public Square find(int x, int y){
 
