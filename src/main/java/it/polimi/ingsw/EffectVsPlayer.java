f2a34854f9004247f0e9d7db76cc61130d7d6b4e
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-04-02 18:34:48 +0200

    Fixed Effect SubClasses

--- src/main/java/it/polimi/ingsw/EffectVsPlayer.java
+++ src/main/java/it/polimi/ingsw/EffectVsPlayer.java
@@ -2,7 +2,15 @@ package it.polimi.ingsw;
 
 import java.util.ArrayList;
 
-public class EffectVsCharacter extends Effect {
+public class EffectVsPlayer extends Effect {
+
+    public EffectVsPlayer(int costBlue, int costRed, int costYellow, String name, ArrayList<Constraint> constraints, ArrayList<Boolean> constraintPositivity, int damage, int mark, boolean addToList, boolean removeFromList) {
+        super(costBlue, costRed, costYellow, name, constraints, constraintPositivity);
+        this.damage = damage;
+        this.mark = mark;
+        this.addToList = addToList;
+        this.removeFromList = removeFromList;
+    }
 
     private int damage,mark;
     private boolean addToList,removeFromList;
