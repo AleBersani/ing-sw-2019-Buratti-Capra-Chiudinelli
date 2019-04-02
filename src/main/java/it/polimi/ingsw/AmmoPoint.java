c3c76be40a872d10b7ee2d60f9c886bc4cc88ec4
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-04-02 17:56:26 +0200

    Fixed Class AmmoPoint

--- src/main/java/it/polimi/ingsw/AmmoPoint.java
+++ src/main/java/it/polimi/ingsw/AmmoPoint.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class AmmoPoint extends Square{
 
-    private AmmoTile;
+    private AmmoTile ammo;
 
     public AmmoPoint(int x, int y, ArrayList<Square> doors, String color) {
         super(x, y, doors, color);
