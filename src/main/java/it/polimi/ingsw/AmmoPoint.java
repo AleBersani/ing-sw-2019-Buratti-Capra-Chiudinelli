2977df96d221c74bd9398e319be257c394881062
Kiudi <andrea.chiudinelli@mail.polimi.it>
2019-04-02 17:41:20 +0200

    Added Point

---
+++ src/main/java/it/polimi/ingsw/AmmoPoint.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+
+public class AmmoPoint extends Square{
+
+    private AmmoTile;
+
+    public AmmoPoint(int x, int y, ArrayList<Square> doors, String color) {
+        super(x, y, doors, color);
+    }
+
+    public void generateAmmo(){
+
+        return;
+    }
+}
