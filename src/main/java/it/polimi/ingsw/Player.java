23a84d523233267740649e24c8f4c7c542fa6b70
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-04-03 16:31:21 +0200

    Fixed Player Class

--- src/main/java/it/polimi/ingsw/Player.java
+++ src/main/java/it/polimi/ingsw/Player.java
@@ -7,8 +7,8 @@ import it.polimi.ingsw.Map.Square;
 import java.util.ArrayList;
 
 public class Player {
-    private int skull, blueAmmo, RedAmmo, yellowAmmo, points, damegeCounter;
-    private ArrayList<Player> damege = new ArrayList<Player>();
+    private int skull, blueAmmo, RedAmmo, yellowAmmo, points, damageCounter;
+    private ArrayList<Player> damage = new ArrayList<Player>();
     private ArrayList<Player> mark = new ArrayList<Player>();
     private ArrayList<PowerUp> powerUps= new ArrayList<PowerUp>();
     private ArrayList<Weapon> weapons= new ArrayList<Weapon>();
@@ -56,7 +56,7 @@ public class Player {
 
         return;
     }
-    public void wound(int damege, Player shooter){
+    public void wound(int damage, Player shooter){
 
         return;
     }
@@ -65,4 +65,131 @@ public class Player {
         return;
     }
 
+    public int getSkull() {
+        return skull;
+    }
+
+    public void setSkull(int skull) {
+        this.skull = skull;
+    }
+
+    public int getBlueAmmo() {
+        return blueAmmo;
+    }
+
+    public void setBlueAmmo(int blueAmmo) {
+        this.blueAmmo = blueAmmo;
+    }
+
+    public int getRedAmmo() {
+        return RedAmmo;
+    }
+
+    public void setRedAmmo(int redAmmo) {
+        RedAmmo = redAmmo;
+    }
+
+    public int getYellowAmmo() {
+        return yellowAmmo;
+    }
+
+    public void setYellowAmmo(int yellowAmmo) {
+        this.yellowAmmo = yellowAmmo;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getDamageCounter() {
+        return damageCounter;
+    }
+
+    public void setDamageCounter(int damageCounter) {
+        this.damageCounter = damageCounter;
+    }
+
+    public ArrayList<Player> getDamage() {
+        return damage;
+    }
+
+    public void setDamage(ArrayList<Player> damage) {
+        this.damage = damage;
+    }
+
+    public ArrayList<Player> getMark() {
+        return mark;
+    }
+
+    public void setMark(ArrayList<Player> mark) {
+        this.mark = mark;
+    }
+
+    public ArrayList<PowerUp> getPowerUps() {
+        return powerUps;
+    }
+
+    public void setPowerUps(ArrayList<PowerUp> powerUps) {
+        this.powerUps = powerUps;
+    }
+
+    public ArrayList<Weapon> getWeapons() {
+        return weapons;
+    }
+
+    public void setWeapons(ArrayList<Weapon> weapons) {
+        this.weapons = weapons;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    public boolean isLastKill() {
+        return lastKill;
+    }
+
+    public void setLastKill(boolean lastKill) {
+        this.lastKill = lastKill;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Square getPosition() {
+        return position;
+    }
+
+    public void setPosition(Square position) {
+        this.position = position;
+    }
+
+    public Square getPreviousPosition() {
+        return previousPosition;
+    }
+
+    public void setPreviousPosition(Square previousPosition) {
+        this.previousPosition = previousPosition;
+    }
 }
