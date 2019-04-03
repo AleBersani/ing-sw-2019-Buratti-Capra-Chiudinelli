5d6c5ee845d8f6065662a1453b389d0b7cf99a7f
Kiudi <andrea.chiudinelli@mail.polimi.it>
2019-04-03 15:27:45 +0200

    fixed player

--- src/main/java/it/polimi/ingsw/EffectVsPlayer.java
+++ src/main/java/it/polimi/ingsw/EffectVsPlayer.java
@@ -4,21 +4,15 @@ import java.util.ArrayList;
 
 public class EffectVsPlayer extends Effect {
 
+    private int damage,mark;
+    private boolean addToList,removeFromList;
+
     public EffectVsPlayer(int costBlue, int costRed, int costYellow, String name, ArrayList<Constraint> constraints, ArrayList<Boolean> constraintPositivity, int damage, int mark, boolean addToList, boolean removeFromList) {
         super(costBlue, costRed, costYellow, name, constraints, constraintPositivity);
         this.damage = damage;
         this.mark = mark;
         this.addToList = addToList;
         this.removeFromList = removeFromList;
-    }
-
-    private int damage,mark;
-    private boolean addToList,removeFromList;
-
-    public EffectVsCharacter(int costBlue, int costRed, int costYellow, String name, ArrayList<Constraint> constraints, ArrayList<Boolean> constraintPositivity, int damage, int mark) {
-        super(costBlue, costRed, costYellow, name, constraints, constraintPositivity);
-        this.damage = damage;
-        this.mark = mark;
     }
 
     public void apply(Player target, Player owner){
