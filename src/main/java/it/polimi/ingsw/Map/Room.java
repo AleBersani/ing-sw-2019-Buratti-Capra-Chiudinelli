3adb3c3a2d5abada1daf72f585909743be946193
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-04-05 15:40:59 +0200

    Partially implemented class Room

--- src/main/java/it/polimi/ingsw/Map/Room.java
+++ src/main/java/it/polimi/ingsw/Map/Room.java
@@ -8,16 +8,27 @@ public class Room {
     private ArrayList<Square> squares= new ArrayList<Square>();
     private Board board;
 
-    public Room(int size, ArrayList<Integer> x, ArrayList<Integer> y, Board board) {
+    public Room(int size, ArrayList<Integer> xAmmo, ArrayList<Integer> yAmmo, ArrayList<Integer> xSpawn, ArrayList<Integer> ySpawn ,ArrayList<String> colors, Board board) {
+        int i;
         this.size = size;
         this.board= board;
-        // TODO
-    }
+        for (i=0; i<xAmmo.size();i++){
+            squares.add(i,new AmmoPoint(xAmmo.get(i),yAmmo.get(i),null ,colors.get(i), this));
+        }
+        for (i=0; i<xSpawn.size();i++){
+            squares.add(i,new SpawnPoint(xSpawn.get(i),ySpawn.get(i),null ,colors.get(i+xAmmo.size()), this));
+        }
+    }  // sitemare doors
 
-    public Square find(int x, int y){
-        // TODO
-        return null;
-    }
+    public Square find(int x, int y) throws Exception {
+        int i;
+        for (i=0; i<squares.size();i++) {
+            if (squares.get(i).getX() == x && squares.get(i).getY() == y) {
+                return squares.get(i);
+            }
+        }
+        throw (new Exception());
+    } // aggiungere una nuova eccezzione
 
     public int getSize() {
         return size;
