fd3203c93daa4585adc6bf08eec35f431eceaf0e
PaoloC96 <paolo2.capra@mail.polimi.it>
2019-04-16 20:07:58 +0200

    Modified canShoot tests in SameRoomTest and MinimumDistanceTest

---
+++ src/test/java/it/polimi/ingsw/Model/Cards/Constraints/MinimumDistanceTest.java
@@ -0,0 +1,63 @@
+package it.polimi.ingsw.Model.Cards.Constraints;
+
+import it.polimi.ingsw.Exception.NotFoundException;
+import it.polimi.ingsw.Model.Map.Board;
+import it.polimi.ingsw.Model.Map.Square;
+import it.polimi.ingsw.Model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumDistanceTest {
+
+    Player owner;
+    Board board;
+    Square target;
+    MinimumDistance test;
+    ArrayList<Square> targets;
+
+    @BeforeEach
+    public void setup() {
+        owner = new Player(true,"blue", "Franco");
+        board = new Board(null,"./resources/Board1.json");
+        targets = new ArrayList<Square>();
+        test = new MinimumDistance(2);
+
+    }
+
+    @Test
+    public void testCanNotShoot(){
+        try {
+            owner.setPosition(board.find(1,1 ));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            target = board.find(2,1);
+            targets.add(target);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        assertEquals(false, test.canShoot(targets,owner));
+    }
+
+    @Test
+    public void CanShoot(){
+        try {
+            owner.setPosition(board.find(1,1 ));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            target = board.find(3,2);
+            targets.add(target);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        assertEquals(true, test.canShoot(targets,owner));
+    }
+
+}
---
+++ src/test/java/it/polimi/ingsw/Model/Cards/Constraints/SameRoomTest.java
@@ -0,0 +1,62 @@
+package it.polimi.ingsw.Model.Cards.Constraints;
+
+import it.polimi.ingsw.Exception.NotFoundException;
+import it.polimi.ingsw.Model.Map.Board;
+import it.polimi.ingsw.Model.Map.Square;
+import it.polimi.ingsw.Model.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SameRoomTest {
+
+    Square target;
+    Player owner;
+    Board board;
+    SameRoom test;
+    ArrayList<Square> targets;
+
+    @BeforeEach
+    public void setup(){
+        board = new Board(null,"./resources/Board1.json");
+        owner = new Player(true,"Yellow","Bruno");
+        targets = new ArrayList<Square>();
+        test = new SameRoom();
+    }
+
+    @Test
+    public void sameRoomTest(){
+        try {
+            owner.setPosition(board.find(1,1));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            target = board.find(1,1);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        targets.add(target);
+        assertEquals(true,test.canShoot(targets,owner));
+    }
+
+    @Test
+    public void notSameRoomTest(){
+        try {
+            owner.setPosition(board.find(1,1));
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            target = board.find(3,3);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        targets.add(target);
+        assertEquals(false,test.canShoot(targets,owner));
+    }
+}
