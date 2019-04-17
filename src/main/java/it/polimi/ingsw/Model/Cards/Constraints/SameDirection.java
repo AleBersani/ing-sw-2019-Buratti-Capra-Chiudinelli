fb3ac27235096d055f4dd50231de6a0b93925199
PaoloC96 <paolo2.capra@mail.polimi.it>
2019-04-17 19:45:23 +0200

    Fixed SameDirection maven problems.

--- src/main/java/it/polimi/ingsw/Model/Cards/Constraints/SameDirection.java
+++ src/main/java/it/polimi/ingsw/Model/Cards/Constraints/SameDirection.java
@@ -2,9 +2,6 @@ package it.polimi.ingsw.Model.Cards.Constraints;
 
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Model.Map.Square;
-import sun.plugin2.message.OverlayWindowMoveMessage;
-
-import java.security.acl.Owner;
 import java.util.ArrayList;
 
 public class SameDirection extends Constraint {
