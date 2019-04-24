2e5e0daca533d8bbdfc2f38959d8c6d96cd5f54f
Kiudi <andrea.chiudinelli@mail.polimi.it>
2019-04-24 15:01:31 +0200

    Added and fixed the TurnTest

--- src/test/java/it/polimi/ingsw/Model/TurnTest.java
+++ src/test/java/it/polimi/ingsw/Model/TurnTest.java
@@ -12,7 +12,7 @@ class TurnTest {
 
     Player test,loser,guest;
     Turn turn;
-    ArrayList<Player> damageList,damageList2,playerList;
+    ArrayList<Player> damageList,damageList2,damageList3,playerList;
     Match testMatch;
 
     @BeforeEach
@@ -22,13 +22,14 @@ class TurnTest {
         loser = new Player(false,"yellow", "Paola");
         playerList = new ArrayList<Player>(Arrays.asList(guest,test,loser));
         testMatch = new Match(playerList,3,5,true,"normal");
-        turn = new Turn(null,false,test,null);
-        damageList = new ArrayList<Player>(Arrays.asList(test,guest,guest,guest,guest,guest,guest,test,test,test,test,null));
-        damageList2 = new ArrayList<Player>(Arrays.asList(guest,guest,guest,guest,guest,guest,guest,test,test,test,test,null));
+        turn = new Turn(null,false,test,testMatch);
+        damageList = new ArrayList<Player>(Arrays.asList(test,guest,guest,guest,guest,guest,guest,test,test,test,test));
+        damageList2 = new ArrayList<Player>(Arrays.asList(guest,guest,guest,guest,guest,guest,guest,test,test,test,test));
+        damageList3 = new ArrayList<Player>(Arrays.asList(test,loser,loser,loser,test,test,test,test,test,test,test,test));
     }
 
     @Test
-    void testAddDead() {
+    public void testAddDead() {
         turn.addDead(loser);
         assertEquals(1,turn.getDeads().size());
         turn.addDead(guest);
@@ -38,32 +39,56 @@ class TurnTest {
     @Test
     void testEndTurn() {
     }
-
+    */
     @Test
-    void testSetPoints() {
+    public void testSetPoint() {
+        testMatch.setTurn(turn);
         loser.setDamage(damageList);
-        assertEquals(12,loser.getDamage().size());
+        assertEquals(11,loser.getDamage().size());
         turn.addDead(loser);
-        turn.setPoints();
+        turn.setPoint();
         assertEquals(8,guest.getPoints());
         assertEquals(7,test.getPoints());
         assertEquals(1,loser.getSkull());
         assertEquals(0,turn.getDeads().size());
         assertEquals(0,loser.getDamage().size());
     }
-
+    //TESTED THE RIGHT POINTS CALCULATION
     @Test
-    void testSetPoints2() {
-        testSetPoints();
+    public void testSetPoint2() {
+        testSetPoint();
         loser.setDamage(damageList2);
-        assertEquals(12,loser.getDamage().size());
+        assertEquals(11,loser.getDamage().size());
         turn.addDead(loser);
-        turn.setPoints();
+        turn.setPoint();
         assertEquals(15,guest.getPoints());
         assertEquals(11,test.getPoints());
         assertEquals(2,loser.getSkull());
         assertEquals(0,turn.getDeads().size());
         assertEquals(0,loser.getDamage().size());
     }
-    */
+    //TESTED THE DOUBLE KILL
+    @Test
+    public void testSetPoint3() {
+        testSetPoint();
+        loser.setDamage(damageList2);
+        guest.setDamage(damageList3);
+        assertEquals(12,guest.getDamage().size());
+        turn.addDead(loser);
+        turn.addDead(guest);
+        turn.setPoint();
+        assertEquals(15,guest.getPoints());
+        assertEquals(21,test.getPoints());
+        assertEquals(6,loser.getPoints());
+        assertEquals(2,loser.getSkull());
+        assertEquals(1,guest.getSkull());
+        assertEquals(0,turn.getDeads().size());
+        assertEquals(0,guest.getDamage().size());
+    }
+    //TESTED THE KILL SHOT TRACK LENGTH
+    @Test
+    public void testSetPoint4(){
+        testSetPoint3();
+        assertEquals(4,testMatch.getKillShotTrack().size());
+    }
 }
