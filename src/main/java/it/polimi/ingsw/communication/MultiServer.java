0995d0812222c40a6425ce00e7744674ca9271ec
Furcanzo <48480530+Furcanzo@users.noreply.github.com>
2019-05-16 10:21:06 +0200

    Removed duplicated code

--- src/main/java/it/polimi/ingsw/communication/MultiServer.java
+++ src/main/java/it/polimi/ingsw/communication/MultiServer.java
@@ -1,20 +1,35 @@
 package it.polimi.ingsw.communication;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MultiServer extends Server {
+public class MultiServer implements Closeable {
 
+    private final int port;
+    private ServerSocket serverSocket;
     private ExecutorService pool;
 
     public MultiServer(int port) {
-        super(port);
+        this.port=port;
         pool = Executors.newCachedThreadPool();
     }
 
-    @Override
+    public void init() throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println(">>> Listening on " + port);
+    }
+
+    public Socket acceptConnection() throws IOException {
+        // blocking call
+        Socket accepted = serverSocket.accept();
+        System.out.println("Connection accepted: " + accepted.getRemoteSocketAddress());
+        return accepted;
+    }
+
     public void lifeCycle() throws IOException {
         init();
         pool.submit(new Gestor(this));
@@ -22,5 +37,9 @@ public class MultiServer extends Server {
             final Socket socket = acceptConnection();
             pool.submit(new ClientHandler(socket,this));
         }
+    }
+
+    public void close() throws IOException {
+        serverSocket.close();
     }
 }
