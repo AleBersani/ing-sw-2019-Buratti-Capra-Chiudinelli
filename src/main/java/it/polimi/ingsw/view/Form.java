bf3b1342a53683e19dba39e55225a20f3ac7a6a2
Kiudi <andrea.chiudinelli@mail.polimi.it>
2019-05-24 19:17:50 +0200

    fixed view

--- src/main/java/it/polimi/ingsw/view/Form.java
+++ src/main/java/it/polimi/ingsw/view/Form.java
@@ -2,31 +2,17 @@ package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.communication.Client;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.effect.BoxBlur;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Form extends Application {
 
     private Client client;
     private Stage stage;
+    private LoginGUI loginGUI = new LoginGUI();
 
     public void setClient(Client client) {
         this.client = client;
@@ -38,106 +24,38 @@ public class Form extends Application {
         client.init();
         client.start();
         this.stage = primaryStage;
-        Image image = new Image("/images/loginForm.jpg");
-        ImageView mv = new ImageView(image);
+
+
         StackPane pane = new StackPane();
         Scene scene = new Scene(pane,1500,1000);
-        Button button = new Button();
-        Button button2 = new Button();
-        TextField username = new TextField();
-        GridPane grid = new GridPane();
-        GridPane.setHalignment(button, HPos.CENTER);
-        Rectangle rectangle = new Rectangle(500,400);
-        Label text = new Label();
-        text.setTextFill(Color.web("#FFD938", 0.8));
-        text.setStyle("-fx-font: 70 Helvetica;");
-        text.setEffect(new DropShadow());
-        Label infoText = new Label();
-        infoText.setStyle("-fx-font: 15 Helvetica;");
-        infoText.setEffect(new DropShadow());
+        stage.setScene(scene);
 
         //stage
         stage.setTitle("Adrenaline");
         stage.getIcons().add(new Image("/images/adrenalineLogo.png"));
-
-        //image
-        mv.fitWidthProperty().bind(pane.widthProperty());
-        mv.fitHeightProperty().bind(pane.heightProperty());
-
-        //grid
-        ColumnConstraints column1 = new ColumnConstraints();
-        column1.setPercentWidth(10);
-        grid.getColumnConstraints().addAll(column1);
-        ColumnConstraints column2 = new ColumnConstraints();
-        column2.setPercentWidth(10);
-        grid.getColumnConstraints().addAll(column2);
-        grid.add(text,0,0,2,1);
-        grid.addRow(1, new Text(""));
-        grid.add(username,0,2,2,1);
-        grid.addRow(3, new Text(""));
-        grid.add(button,0,4);
-        grid.add(button2,1,4);
-        grid.add(infoText,0,5,2,1);
-        grid.setAlignment(Pos.CENTER);
-
-        //text
-        GridPane.setHalignment(text, HPos.CENTER);
-        text.setAlignment(Pos.CENTER);
-        text.setText("Login");
-        text.prefWidthProperty().bind(pane.widthProperty().divide(5));
-
-        //info text
-        GridPane.setHalignment(infoText, HPos.CENTER);
-        infoText.setAlignment(Pos.CENTER);
-        infoText.prefWidthProperty().bind(pane.widthProperty().divide(7));
-        infoText.prefHeightProperty().bind(pane.heightProperty().divide(7));
-
-        //username
-        username.setPromptText("Username");
-        username.prefWidthProperty().bind(pane.widthProperty().divide(20));
-        username.prefHeightProperty().bind(pane.heightProperty().divide(20));
-
-        //button
-        GridPane.setHalignment(button, HPos.CENTER);
-        button.setAlignment(Pos.CENTER);
-        button.setText("LOGIN");
-        button.prefWidthProperty().bind(pane.widthProperty().divide(15));
-        button.prefHeightProperty().bind(pane.heightProperty().divide(22));
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                client.send("login " + username.getText());
-                if(username.getText().equals("prova")) {
-                    infoText.setTextFill(Color.web("#66ff66", 0.8));
-                    infoText.setText("Successfully logged");
-                }
-                else {
-                    infoText.setTextFill(Color.web("#ff0000", 0.8));
-                    infoText.setText("Username already in use");
-                }
-            }
-        });
-
-        //button2
-        GridPane.setHalignment(button2, HPos.CENTER);
-        button2.setAlignment(Pos.CENTER);
-        button2.setText("EXIT");
-        button2.prefWidthProperty().bind(pane.widthProperty().divide(15));
-        button2.prefHeightProperty().bind(pane.heightProperty().divide(22));
-
-
-        //rectangle
-        rectangle.setFill(Color.rgb(0, 0, 0, 0.5));
-        rectangle.setEffect(new BoxBlur());
-        rectangle.widthProperty().bind(pane.widthProperty());
-        rectangle.heightProperty().bind(pane.heightProperty().divide(2));
-
-        pane.getChildren().add(mv);
-        pane.getChildren().add(rectangle);
-        pane.getChildren().add(grid);
-
-        stage.setScene(scene);
         stage.setResizable(true);
+
+        //call methods
+        /*
+        for(int i=0;i<3;i++){
+            StackPane stackPane = new StackPane();
+            stage.getScene().setRoot(stackPane);
+            loginGUI.loginImageSetting(stage);
+            if(i==0){
+                loginGUI.loginGridSetting(stage,client);
+            }
+            if(i==1){
+                loginGUI.menuGridSetting(stage);
+            }
+            if(i==2){
+                loginGUI.roomGridSetting(stage);
+            }
+            stage.show();
+        }
+        */
+        loginGUI.loginImageSetting(stage);
+        loginGUI.loginGridSetting(stage,client);
+
         stage.show();
     }
 
---
+++ src/main/java/it/polimi/ingsw/view/LoginGUI.java
@@ -0,0 +1,342 @@
+package it.polimi.ingsw.view;
+
+import it.polimi.ingsw.communication.Client;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class LoginGUI {
+
+    public void loginImageSetting(Stage stage){
+        Image image = new Image("/images/loginForm.jpg");
+        ImageView mv = new ImageView(image);
+        Rectangle rectangle = new Rectangle(500,400);
+        StackPane pane = (StackPane) stage.getScene().getRoot();
+        stage.getScene().setRoot(pane);
+
+        //image
+        mv.fitWidthProperty().bind(pane.widthProperty());
+        mv.fitHeightProperty().bind(pane.heightProperty());
+
+        //rectangle
+        rectangle.setFill(Color.rgb(0, 0, 0, 0.5));
+        rectangle.setEffect(new BoxBlur());
+        rectangle.widthProperty().bind(pane.widthProperty());
+        rectangle.heightProperty().bind(pane.heightProperty().divide(2));
+
+        //pane
+        pane.getChildren().add(mv);
+        pane.getChildren().add(rectangle);
+
+        //stage
+        stage.setScene(stage.getScene());
+    }
+
+    public void loginGridSetting(Stage stage, Client client){
+        StackPane pane = (StackPane)stage.getScene().getRoot();
+        stage.getScene().setRoot(pane);
+        Button button = new Button();
+        Button button2 = new Button();
+        TextField username = new TextField();
+        GridPane grid = new GridPane();
+        Label text = new Label();
+        text.setTextFill(Color.web("#FFD938", 0.8));
+        text.setStyle("-fx-font: 70 Helvetica;");
+        text.setEffect(new DropShadow());
+        Label infoText = new Label();
+        infoText.setStyle("-fx-font: 15 Helvetica;");
+        infoText.setEffect(new DropShadow());
+
+        //grid
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(10);
+        grid.getColumnConstraints().addAll(column1);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(10);
+        grid.getColumnConstraints().addAll(column2);
+        grid.add(text,0,0,2,1);
+        grid.addRow(1, new Text(""));
+        grid.add(username,0,2,2,1);
+        grid.addRow(3, new Text(""));
+        grid.add(button,0,4);
+        grid.add(button2,1,4);
+        grid.add(infoText,0,5,2,1);
+        grid.setAlignment(Pos.CENTER);
+
+        //text
+        GridPane.setHalignment(text, HPos.CENTER);
+        text.setAlignment(Pos.CENTER);
+        text.setText("Login");
+        text.prefWidthProperty().bind(pane.widthProperty().divide(5));
+
+        //info text
+        GridPane.setHalignment(infoText, HPos.CENTER);
+        infoText.setAlignment(Pos.CENTER);
+        infoText.prefWidthProperty().bind(pane.widthProperty().divide(7));
+        infoText.prefHeightProperty().bind(pane.heightProperty().divide(7));
+
+        //username
+        username.setPromptText("Username");
+        username.prefWidthProperty().bind(pane.widthProperty().divide(20));
+        username.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //button
+        GridPane.setHalignment(button, HPos.CENTER);
+        button.setAlignment(Pos.CENTER);
+        button.setText("LOGIN");
+        button.prefWidthProperty().bind(pane.widthProperty().divide(15));
+        button.prefHeightProperty().bind(pane.heightProperty().divide(22));
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                client.send("login " + username.getText());
+                if(username.getText().equals("prova")) {
+                    infoText.setTextFill(Color.web("#66ff66", 0.8));
+                    infoText.setText("Successfully logged");
+                }
+                else {
+                    infoText.setTextFill(Color.web("#ff0000", 0.8));
+                    infoText.setText("Username already in use");
+                }
+            }
+        });
+
+        //button2
+        GridPane.setHalignment(button2, HPos.CENTER);
+        button2.setAlignment(Pos.CENTER);
+        button2.setText("EXIT");
+        button2.prefWidthProperty().bind(pane.widthProperty().divide(15));
+        button2.prefHeightProperty().bind(pane.heightProperty().divide(22));
+
+        pane.getChildren().add(grid);
+    }
+
+    public void menuGridSetting(Stage stage){
+        StackPane pane = (StackPane)stage.getScene().getRoot();
+        stage.getScene().setRoot(pane);
+        GridPane grid = new GridPane();
+        Button button = new Button("DONE");
+        Button button2 = new Button("EXIT");
+
+        //title label
+        Label title = new Label();
+        title.setTextFill(Color.web("#FFD938", 0.8));
+        title.setStyle("-fx-font: 40 Helvetica;");
+        title.setEffect(new DropShadow());
+
+        //label info menu
+        Label infoMenu = new Label("Board");
+        infoMenu.setTextFill(Color.web("#FFD938", 0.8));
+        infoMenu.setStyle("-fx-font: 30 Helvetica;");
+        infoMenu.setEffect(new DropShadow());
+        GridPane.setHalignment(infoMenu, HPos.CENTER);
+
+        //label info menu 2
+        Label infoMenu2 = new Label("Skulls");
+        infoMenu2.setTextFill(Color.web("#FFD938", 0.8));
+        infoMenu2.setStyle("-fx-font: 30 Helvetica;");
+        infoMenu2.setEffect(new DropShadow());
+        GridPane.setHalignment(infoMenu2, HPos.CENTER);
+
+        //label info menu 3
+        Label infoMenu3 = new Label("Frenzy");
+        infoMenu3.setTextFill(Color.web("#FFD938", 0.8));
+        infoMenu3.setStyle("-fx-font: 30 Helvetica;");
+        infoMenu3.setEffect(new DropShadow());
+        GridPane.setHalignment(infoMenu3, HPos.CENTER);
+
+        //info text
+        Label infoText = new Label();
+        infoText.setStyle("-fx-font: 20 Helvetica;");
+        infoText.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        infoText.prefHeightProperty().bind(pane.heightProperty().divide(10));
+        infoText.setEffect(new DropShadow());
+
+        //TODO AUTOMATE GETITEMS
+
+        //choice box 2
+        ChoiceBox title2 = new ChoiceBox();
+        title2.getItems().addAll("1","2");
+        title2.setTooltip(new Tooltip("Select a board"));
+
+        //choice box 3
+        ChoiceBox title3 = new ChoiceBox();
+        title3.getItems().addAll("5","8");
+        title3.setTooltip(new Tooltip("Select the number of skulls"));
+
+        //choice box 4
+        ChoiceBox title4 = new ChoiceBox();
+        title4.getItems().addAll("Yes","No");
+        title4.setTooltip(new Tooltip("Select if you want to enable frenzy"));
+
+        //grid
+        grid.add(title,0,0,5,1);
+        grid.addRow(1,new Text(""));
+        grid.add(infoMenu,0,2);
+        grid.add(infoMenu2,2,2);
+        grid.add(infoMenu3,4,2);
+        grid.addRow(3,new Text(""));
+        grid.add(title2,0,4);
+        grid.add(button,1,6);
+        grid.add(title3,2,4);
+        grid.add(button2,3,6);
+        grid.add(title4,4,4);
+        grid.addRow(5,new Text("\n\n"));
+        grid.add(infoText,0,7,5,1);
+        grid.setAlignment(Pos.CENTER);
+
+        //title
+        GridPane.setHalignment(title, HPos.CENTER);
+        title.setAlignment(Pos.CENTER);
+        title.setText("Choose the settings of the game");
+        title.prefWidthProperty().bind(pane.widthProperty().divide(2));
+
+        //title2
+        GridPane.setHalignment(title2, HPos.CENTER);
+        title2.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title2.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title3
+        GridPane.setHalignment(title3, HPos.CENTER);
+        title3.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title3.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title4
+        GridPane.setHalignment(title4, HPos.CENTER);
+        title4.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title4.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //button
+        GridPane.setHalignment(button, HPos.CENTER);
+        button.setAlignment(Pos.CENTER);
+        button.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        button.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //button2
+        GridPane.setHalignment(button2, HPos.CENTER);
+        button2.setAlignment(Pos.CENTER);
+        button2.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        button2.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        pane.getChildren().add(grid);
+    }
+
+    public void roomGridSetting(Stage stage){
+        StackPane pane = (StackPane)stage.getScene().getRoot();
+        stage.getScene().setRoot(pane);
+        GridPane grid = new GridPane();
+
+        //title label
+        Label title = new Label();
+        title.setTextFill(Color.web("#FFD938", 0.8));
+        title.setStyle("-fx-font: 50 Helvetica;");
+        title.setEffect(new DropShadow());
+
+        //title nickname player 1
+        Label title1 = new Label();
+        title1.setTextFill(Color.web("#FFD938", 0.8));
+        title1.setStyle("-fx-font: 20 Helvetica;");
+        title1.setEffect(new DropShadow());
+
+        //title nickname player 2
+        Label title2 = new Label();
+        title2.setTextFill(Color.web("#FFD938", 0.8));
+        title2.setStyle("-fx-font: 20 Helvetica;");
+        title2.setEffect(new DropShadow());
+
+        //title nickname player 3
+        Label title3 = new Label();
+        title3.setTextFill(Color.web("#FFD938", 0.8));
+        title3.setStyle("-fx-font: 20 Helvetica;");
+        title3.setEffect(new DropShadow());
+
+        //title nickname player 4
+        Label title4 = new Label();
+        title4.setTextFill(Color.web("#FFD938", 0.8));
+        title4.setStyle("-fx-font: 20 Helvetica;");
+        title4.setEffect(new DropShadow());
+
+        //title nickname player 5
+        Label title5 = new Label();
+        title5.setTextFill(Color.web("#FFD938", 0.8));
+        title5.setStyle("-fx-font: 20 Helvetica;");
+        title5.setEffect(new DropShadow());
+
+        //button
+        Button button = new Button("EXIT");
+
+        //grid
+        grid.add(title,0,0);
+        grid.addRow(1,new Text (""));
+        grid.add(title1,0,2);
+        grid.add(title2,0,3);
+        grid.add(title3,0,4);
+        grid.add(title4,0,5);
+        grid.add(title5,0,6);
+        grid.addRow(7,new Text (""));
+        grid.add(button,0,8);
+        grid.setAlignment(Pos.CENTER);
+
+        //title
+        GridPane.setHalignment(title, HPos.CENTER);
+        title.setAlignment(Pos.CENTER);
+        title.setText("Player");
+        title.prefWidthProperty().bind(pane.widthProperty().divide(2));
+
+        //title1
+        GridPane.setHalignment(title1, HPos.CENTER);
+        title1.setAlignment(Pos.CENTER);
+        title1.setText("Roberto");
+        title1.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title1.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title2
+        GridPane.setHalignment(title2, HPos.CENTER);
+        title2.setAlignment(Pos.CENTER);
+        title2.setText("Paolo");
+        title2.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title2.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title3
+        GridPane.setHalignment(title3, HPos.CENTER);
+        title3.setAlignment(Pos.CENTER);
+        title3.setText("Andrea");
+        title3.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title3.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title4
+        GridPane.setHalignment(title4, HPos.CENTER);
+        title4.setAlignment(Pos.CENTER);
+        title4.setText("Filippo");
+        title4.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title4.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //title5
+        GridPane.setHalignment(title5, HPos.CENTER);
+        title5.setAlignment(Pos.CENTER);
+        title5.setText("Elisa");
+        title5.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        title5.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        //button
+        GridPane.setHalignment(button, HPos.CENTER);
+        button.setAlignment(Pos.CENTER);
+        button.prefWidthProperty().bind(pane.widthProperty().divide(10));
+        button.prefHeightProperty().bind(pane.heightProperty().divide(20));
+
+        pane.getChildren().add(grid);
+    }
+}
