8b7329d6de09b9e750b6cb5021ce22d413d037cf
Kiudi <andrea.chiudinelli@mail.polimi.it>
2019-05-29 19:03:49 +0200

    working on gui

---
+++ src/main/java/it/polimi/ingsw/view/gui/GameGUI.java
@@ -0,0 +1,140 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class GameGUI extends Application {
+
+    private Stage stage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane pane = new StackPane();
+        this.stage = primaryStage;
+        Scene scene = new Scene(pane, Toolkit.getDefaultToolkit().getScreenSize().getWidth(),Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+        stage.setFullScreenExitHint("");
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+
+        GridPane grid = new GridPane();
+
+        Image black = new Image("/images/game/greyCell.png");
+        ImageView blackScreen = new ImageView(black);
+        Image blue = new Image("/images/game/blueCell.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image red = new Image("/images/game/redCell.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image yellow = new Image("/images/game/yellowCell.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image white = new Image("/images/game/whiteCell.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image black2 = new Image("/images/game/blackCell.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+
+        Image wallW = new Image("/images/game/wallW.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image wallN = new Image("/images/game/wallN.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image wallS = new Image("/images/game/wallS.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image wallE = new Image("/images/game/wallE.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image doorW = new Image("/images/game/doorW.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image doorE = new Image("/images/game/doorE.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image doorN = new Image("/images/game/doorN.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+        Image doorS = new Image("/images/game/doorS.png",pane.getWidth()/8,pane.getHeight()/4,false,false);
+
+        Image bluePlayer = new Image("/images/game/plance/bluePlayer.png",pane.getWidth()/2,pane.getHeight()/4,false,false);
+        Image yellowPlayer = new Image("/images/game/plance/yellowPlayer.png",pane.getWidth()/2,pane.getHeight()/4,false,false);
+        Image greenPlayer = new Image("/images/game/plance/greenPlayer.png",pane.getWidth()/2,pane.getHeight()/4,false,false);
+        Image greyPlayer = new Image("/images/game/plance/greyPlayer.png",pane.getWidth()/2,pane.getHeight()/4,false,false);
+        Image purplePlayer = new Image("/images/game/plance/purplePlayer.png",pane.getWidth()/2,pane.getHeight()/4,false,false);
+
+        //image
+        blackScreen.fitWidthProperty().bind(pane.widthProperty());
+        blackScreen.fitHeightProperty().bind(pane.heightProperty());
+
+        //grid
+
+        //grid column constraint
+        for (int j = 0 ; j < 8; j++) {
+            ColumnConstraints col = new ColumnConstraints();
+            col.setHgrow(Priority.ALWAYS);
+            grid.getColumnConstraints().add(col);
+        }
+
+        //grid row constraint
+        for (int i = 0 ; i < 4; i++) {
+            RowConstraints row = new RowConstraints();
+            row.setVgrow(Priority.ALWAYS);
+            grid.getRowConstraints().add(row);
+        }
+
+        //grid
+        grid.add(new ImageView(blue),0,0);
+        grid.add(new ImageView(wallW),0,0);
+        grid.add(new ImageView(wallN),0,0);
+        grid.add(new ImageView(doorS),0,0);
+
+        grid.add(new ImageView(blue),1,0);
+        grid.add(new ImageView(wallN),1,0);
+        grid.add(new ImageView(wallS),1,0);
+
+        grid.add(new ImageView(blue),2,0);
+        grid.add(new ImageView(wallN),2,0);
+        grid.add(new ImageView(wallE),2,0);
+        grid.add(new ImageView(doorS),2,0);
+
+        grid.add(new ImageView(black2),3,0);
+
+        grid.add(new ImageView(red),0,1);
+        grid.add(new ImageView(wallW),0,1);
+        grid.add(new ImageView(wallS),0,1);
+        grid.add(new ImageView(doorN),0,1);
+
+        grid.add(new ImageView(red),1,1);
+        grid.add(new ImageView(wallN),1,1);
+        grid.add(new ImageView(doorS),1,1);
+
+        grid.add(new ImageView(red),2,1);
+        grid.add(new ImageView(wallS),2,1);
+        grid.add(new ImageView(doorE),2,1);
+        grid.add(new ImageView(doorN),2,1);
+
+        grid.add(new ImageView(black2),0,2);
+
+        grid.add(new ImageView(white),1,2);
+        grid.add(new ImageView(wallW),1,2);
+        grid.add(new ImageView(wallS),1,2);
+        grid.add(new ImageView(doorN),1,2);
+
+        grid.add(new ImageView(white),2,2);
+        grid.add(new ImageView(wallN),2,2);
+        grid.add(new ImageView(wallS),2,2);
+        grid.add(new ImageView(doorE),2,2);
+
+        grid.add(new ImageView(yellow),3,1);
+        grid.add(new ImageView(wallN),3,1);
+        grid.add(new ImageView(wallE),3,1);
+        grid.add(new ImageView(doorW),3,1);
+
+        grid.add(new ImageView(yellow),3,2);
+        grid.add(new ImageView(wallS),3,2);
+        grid.add(new ImageView(wallE),3,2);
+        grid.add(new ImageView(doorW),3,2);
+
+        grid.add(new ImageView(yellowPlayer),4,0,4,1);
+        grid.add(new ImageView(greenPlayer),4,1,4,1);
+        grid.add(new ImageView(greyPlayer),4,2,4,1);
+        grid.add(new ImageView(purplePlayer),4,3,4,1);
+        grid.add(new ImageView(bluePlayer),0,3,4,1);
+
+        //pane
+        pane.getChildren().add(blackScreen);
+        pane.getChildren().add(grid);
+
+        //stage
+        stage.show();
+    }
+}
