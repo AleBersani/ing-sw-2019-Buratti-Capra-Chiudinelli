303f1f071945cf86976ff8032620bd2cad63df0a
PaoloC96 <paolo2.capra@mail.polimi.it>
2019-07-04 19:30:15 +0200

    javadoc

--- src/main/java/it/polimi/ingsw/communication/client/Client.java
+++ src/main/java/it/polimi/ingsw/communication/client/Client.java
@@ -7,19 +7,51 @@ import javafx.application.Application;
 import java.io.*;
 import java.net.Socket;
 
+/**
+ * class of the Client
+ */
 public class Client extends Thread implements Closeable {
+    /**
+     * This attribute is the IP of the server
+     */
     private static String host;
+    /**
+     * This attribute is the port of the server
+     */
     private static int port;
+    /**
+     * This attribute is the socket that is used for the connection
+     */
     private Socket connection;
+    /**
+     * This attribute is the buffered reader that is used for reading the input
+     */
     private BufferedReader in;
+    /**
+     * This attribute is the print writer that is used to write the output
+     */
     private PrintWriter out;
+    /**
+     * This attribute is the Interface of the view
+     */
     private ViewInterface view;
+    /**
+     * This attribute is the reference to the Message Handler
+     */
     private MessageHandler messageHandler;
 
+    /**
+     * Constructor method of the Client
+     * @param view the view
+     */
     public Client(ViewInterface view) {
         this.view = view;
     }
 
+    /**
+     * Main method of the client that launch the GUI
+     * @param args parameters from command line
+     */
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Provide host:port please");
@@ -38,13 +70,15 @@ public class Client extends Thread implements Closeable {
         Application.launch(GUI.class,args);
     }
 
+    /**
+     * This run constantly receive messages from the server and if the Server send null this method request the close process of the Client
+     */
     @Override
     public void run(){
         String received;
         try {
             do {
                 received = in.readLine();
-                System.out.println(received); //sarà da togliere
                 if(received != null) {
                     messageHandler.understandMessage(received);
                 }
@@ -65,11 +99,18 @@ public class Client extends Thread implements Closeable {
 
     }
 
+    /**
+     * This method send messages to the Server
+     * @param message message to send
+     */
     public synchronized void send(String message) {
         out.println(message);
-        System.out.println("+++++++++".concat(message));
     }
 
+    /**
+     * This method close the Client and send a message of quit to the Server
+     * @throws IOException if fails the closing process of the socket connection
+     */
     public void close() throws IOException {
         this.send("quit");
         in.close();
@@ -82,13 +123,20 @@ public class Client extends Thread implements Closeable {
         }
     }
 
+    /**
+     * Initializer method of the socket connection
+     * @throws IOException if the server is not already open
+     */
     public void init() throws IOException {
         connection = new Socket(host, port);
         in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         out = new PrintWriter(connection.getOutputStream(), true);
     }
 
-
+    /**
+     * Setter method of MessageHandler
+     * @param messageHandler the MessageHandler to set
+     */
     public void setMessageHandler(MessageHandler messageHandler) {
         this.messageHandler = messageHandler;
     }
--- src/main/java/it/polimi/ingsw/communication/client/MessageHandler.java
+++ src/main/java/it/polimi/ingsw/communication/client/MessageHandler.java
@@ -6,27 +6,69 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+/**
+ * This class understand the message and request a specific action to the view
+ */
 public class MessageHandler {
+    /**
+     * message to show in every moment of the game
+     */
     private String toShow;
+    /**
+     * list of player from the server
+     */
     private String[] bigReceive;
+    /**
+     * reference to the view
+     */
     private ViewInterface view;
+    /**
+     * reference to the Client
+     */
     private Client client;
+    /**
+     * actual state of the game
+     */
     private State state;
+    /**
+     * true if this client is suspended for inactivity
+     */
     private boolean suspend = false;
+    /**
+     * true if the game is finished
+     */
     private boolean endGame = false;
+    /**
+     * constant for the etiquette of the messages to show
+     */
     private static final int TO_SHOW_ETIQUETTE = 3;
+    /**
+     * constant for the etiquette of every message
+     */
     private static final int NAME_ETIQUETTE = 4;
 
+    /**
+     * represent the different states of the game
+     */
     public enum State{
         LOGIN, MENU, WAIT, GAME, END_GAME
     }
 
+    /**
+     * Constructor method of MessageHandler
+     * @param view the view of the client
+     * @param client the client
+     */
     public MessageHandler(ViewInterface view, Client client) {
         this.view = view;
         this.client = client;
         this.state = State.LOGIN;
     }
 
+    /**
+     * first method to separate the message from the server using the state of the game
+     * @param msg the message from the server
+     */
     synchronized void understandMessage(String msg){
         if(!suspend) {
             if ((msg.startsWith(">>>"))&&(!endGame)) {
@@ -58,6 +100,10 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * understander method of the Login state
+     * @param msg the message from the server
+     */
     private void loginUnderstand(String msg){
         switch (msg){
             case "Login":{
@@ -81,6 +127,10 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * understander method of the Menu state
+     * @param msg the message from the server
+     */
     private void menuUnderstand(String msg){
         String[] stringo = msg.split("-");
         switch (stringo[0]){
@@ -109,6 +159,10 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * method to split the message of the player list
+     * @param msg the message from the server
+     */
     private void waitUnderstand(String msg){
         if(msg.substring(0,TO_SHOW_ETIQUETTE).equals(":::")){
             bigReceive = msg.substring(NAME_ETIQUETTE).split("-");
@@ -119,6 +173,10 @@ public class MessageHandler {
         }
     }
 
+    /**
+     * understander method of the Game state
+     * @param msg the message from the server
+     */
     private void gameUnderstand(String msg){
         switch (msg.substring(0,NAME_ETIQUETTE)){
             case "BGD-":{
@@ -185,26 +243,45 @@ public class MessageHandler {
         }
     }
 
-
-
+    /**
+     * method to split the menu parameters
+     * @param msg the message from the server
+     * @return the list of setting to show
+     */
     private ArrayList<String> stringToArrayList(String msg){
         ArrayList<String> data = new ArrayList<>();
         Collections.addAll(data, msg.substring(1, msg.length() - 1).split(","));
         return data;
     }
 
+    /**
+     * Getter method of State
+     * @return the state of the game
+     */
     public State getState() {
         return state;
     }
 
+    /**
+     * getter method of Big Receive
+     * @return return the list of String
+     */
     public String[] getBigReceive() {
         return bigReceive;
     }
 
+    /**
+     * getter method of ToShow
+     * @return the message to show
+     */
     public String getToShow() {
         return toShow;
     }
 
+    /**
+     * setter method of suspended
+     * @param suspend true if the player is suspended
+     */
     public void setSuspend(boolean suspend) {
         this.suspend = suspend;
     }
